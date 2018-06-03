2218c4f3e38a340a8dedc87c3b7808a95cd826a0
uniquexiaobai <unique_xiaobai@qq.com>
2018-06-03 22:17:44 +0800

    add account/login

---
+++ router/account.js
@@ -0,0 +1,70 @@
+const AV = require('leancloud-storage');
+const express = require('express');
+const axios = require('axios');
+const router = express.Router();
+
+const conf = require('../conf.json');
+
+AV.init({
+    appId: conf.leancloud.APP_ID,
+    appKey: conf.leancloud.APP_KEY
+});
+
+router.post('/login', (req, res, next) => {
+    const {loginType, openid, access_token, oauth_consumer_key, expires_in} = req.body;
+    const auth = {
+        openid, 
+        access_token, 
+        oauth_consumer_key, 
+        expires_in,
+    };
+
+    login(loginType, auth)
+        .then(user => {
+            res.json({
+                code: 1,
+                data: user,
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            next(err);
+        });
+});
+
+const getQQInfo = async ({openid, access_token, oauth_consumer_key}) => {
+    const url = `https://graph.qq.com/user/get_user_info?access_token=${access_token}&oauth_consumer_key=${oauth_consumer_key}&openid=${openid}`;
+    
+    try {
+        const {data = {}} = await axios.get(url) || {};
+
+        if (data.ret) {
+            throw data.msg;
+        }
+        const {nickname, gender, figureurl_qq_2} = data; 
+        return {nickname, gender, figureurl: figureurl_qq_2};
+    } catch (err) {
+        throw err;
+    }
+};
+
+const login = async (loginType, {openid, access_token, oauth_consumer_key}) => {
+    try {
+        const {nickname, gender, figureurl} = await getQQInfo({openid, access_token, oauth_consumer_key});
+        const loggedInUser = await AV.User.loginWithAuthData({
+            openid,
+            access_token,
+        }, loginType);
+
+        loggedInUser.set('nickname', nickname);
+        loggedInUser.set('gender', gender);
+        loggedInUser.set('figureurl', figureurl);
+        const user = await loggedInUser.save();
+
+        return user;
+    } catch (err) {
+        throw err;
+    }
+};
+
+module.exports = router;
--- router/index.js
+++ router/index.js
@@ -1,9 +1,11 @@
 const newsRouter = require('./news');
 const libraryRouter = require('./library');
 const educationRouter = require('./education');
+const accountRouter = require('./account');
 
 module.exports = (app) => {
   app.use('/news', newsRouter);
   app.use('/library', libraryRouter);
   app.use('/education', educationRouter);
+  app.use('/account', accountRouter);
 };
